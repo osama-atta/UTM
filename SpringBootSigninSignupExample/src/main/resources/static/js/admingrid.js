88f82520c94ac50710cbea79d9c9b46c68bb1c16
jlgge <jlgge@lotus>
2021-04-23 20:53:49 -0500

    update admin grid working

--- SpringBootSigninSignupExample/src/main/resources/static/js/admingrid.js
+++ SpringBootSigninSignupExample/src/main/resources/static/js/admingrid.js
@@ -2,13 +2,11 @@
  * 
  */
 new gridjs.Grid({
-
- 	  columns: ['Employee Id', 'Comments', 'datesubmitted', "endtime", "starttime"],
-		  server: {
-		    url: 'http://localhost:8080/UTM/timesheets',
-		    then: data => data.results.map(timesheet => 
-		      [timesheet.employeeid, timesheet.comments, timesheet.datesubmitted timesheet.endtime, timesheet.starttime]
-		    )
-		  } 
-	  
+  columns: ['Employee Id', 'Date', 'Comments'],
+  server: {
+    url: 'http://localhost:8080/UTM/timesheets/',
+    then: data => data.map(movie => 
+      [movie.employeeid, movie.date, movie.comments]
+    )
+  } 
 }).render(document.getElementById("wrapper"));
