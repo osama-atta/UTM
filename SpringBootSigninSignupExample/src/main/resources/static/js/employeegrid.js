08406994b298bb6e413c0b00c10dd9db69ffb10b
jlgge <jlgge@lotus>
2021-04-24 07:30:09 -0500

    updated admin page with new grid. I don't have the admin passwordk with me to check this. It works on my demo page. I am on my laptop in KC>

---
+++ SpringBootSigninSignupExample/src/main/resources/static/js/employeegrid.js
@@ -0,0 +1,12 @@
+/**
+ * Simple employee timesheet. I will add more when I get here the add functions etc.
+ */
+new gridjs.Grid({
+  columns: ['Employee Id', 'Date', 'Comments', "Date Submitted", "Start Time", "End Time"],
+  server: {
+    url: 'http://localhost:8080/UTM/timesheet/6',
+    then: data => data.map(timesheet => 
+      [timesheet.employeeid, timesheet.date, timesheet.comments, timesheet.datesubmitted, timesheet.starttime, timesheet.endtime]
+    )
+  } 
+}).render(document.getElementById("wrapper"));
